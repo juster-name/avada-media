147524d1b206121d0a505396bced9f49b0c54c78
juster-name <vladlen044@gmail.com>
2021-10-12 19:08:05 +0300

    Классы Circle и Rectangle

---
+++ patterns/Prototype/Figures.php
@@ -0,0 +1,32 @@
+<?php
+require_once "Shape.php";
+
+class Circle extends Shape
+{
+    public float $radius;
+
+    public function Circle(float $radius)
+    {
+        $this->radius = $radius;
+    }
+    public function clone() : Shape
+    {
+        return new Circle($this->radius);
+    }
+}
+
+class Rectangle extends Shape
+{
+    public float $height, $width;
+
+    public function Circle(int $height, int $width)
+    {
+        $this->radius = $radius;
+    }
+    public function clone() : Shape
+    {
+        return new Circle($this->radius);
+    }
+}
+
+?>
