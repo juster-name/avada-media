7751d634f31079274fa40cc24d4915a12ed9e658
juster-name <vladlen044@gmail.com>
2021-10-16 21:49:30 +0300

    minor /n

--- patterns/Behavioral/Chain of Responsibility/main.php
+++ patterns/Behavioral/Chain of Responsibility/main.php
@@ -6,6 +6,6 @@ $chainHandler = new KeyboardHandler(new MouseHandler(new SensorHandler(null)));
 echo $chainHandler->handle("sensor")."\n";
 echo $chainHandler->handle("keyboard")."\n";
 echo $chainHandler->handle("mouse")."\n";
-echo $chainHandler->handle("gamepad")."\n";
+echo $chainHandler->handle("gamepad");
 
 ?>
