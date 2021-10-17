062e9ad1129c506cdbd0b109be7817d986bf5b0e
juster-name <vladlen044@gmail.com>
2021-10-17 09:47:09 +0300

    adapted agregation

--- patterns/Structural/Adapter.php
+++ patterns/Structural/Adapter.php
@@ -11,27 +11,31 @@ class Adapter
         $this->adaptee = $adaptee;
     }
   
-    public function contact20() : string
+    public function contact20(Target $adapter) : string
     {
         $this->adaptee->connectPin10("20");
+        $adapter->contact20("10");
         return "Adapter has connected 'Contact20' to 'Pin10''\n";
     }
 
-    public function contact21() : string
+    public function contact21(Target $adapter) : string
     {
         $this->adaptee->connectPin11("21");
+        $adapter->contact20("11");
         return "Adapter has connected 'Contact21' to 'Pin11''\n";
     }
 
-    public function contact22() : string
+    public function contact22(Target $adapter) : string
     {
         $this->adaptee->connectGround("ground");
+        $adapter->contact22("ground");
         return "Adapter has connected 'Contact22' to 'Ground'\n";
     }
 
-    public function ground() : string
+    public function ground(Target $adapter) : string
     {
         $this->adaptee->connectGround("ground");
+        $adapter->ground("ground");
         return "Adapter has connected 'Contact22' to 'Ground'\n";
     }
 }
