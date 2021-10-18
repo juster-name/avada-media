83ecc4f69ecd08544b275dad61b68516722b7641
juster-name <vladlen044@gmail.com>
2021-10-18 22:20:38 +0300

    example

--- patterns/Structural/Bridge/main.php
+++ patterns/Structural/Bridge/main.php
@@ -1,7 +1,25 @@
 <?php
-require_once "RefinedAbstraction.php";
+require_once "ConcreteAbstractions.php";
+require_once "ConcreteImplementation.php";
 
+$implementation = new ConcreteImplementation();
 
+$abstractionA = new ConcreteAbstractionA($implementation);
+$abstractionB = new ConcreteAbstractionB($implementation);
+
+echo "ConcreteAbstractionA Test:\n";
+
+$abstractionA->featureA();
+$abstractionA->featureB();
+$abstractionA->absFeature();
+$abstractionA->newFeatureA();
+
+echo "\nConcreteAbstractionB Test:\n";
+
+$abstractionB->featureA();
+$abstractionB->featureB();
+$abstractionB->absFeature();
+$abstractionB->newFeatureB();
 
 
 ?>
